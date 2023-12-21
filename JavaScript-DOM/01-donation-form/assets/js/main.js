534735e2ba119e1351997d5dfab94e914571310e
yrd369 <yogharajdhayalnamburajan@gmail.com>
2023-12-21 11:12:53 +0530

    feat: added greeting feature

--- JavaScript-DOM/01-donation-form/assets/js/main.js
+++ JavaScript-DOM/01-donation-form/assets/js/main.js
@@ -1,12 +1,29 @@
-const firstNameEl = document.querySelector("input#fNameIn");
 const pEl = document.querySelector("p#greet");
-const allDivEl = document.querySelector("form");
+const mainEl = document.querySelector("form");
 const formEL = document.forms.donation;
-const { firstName, lastName, email, amount, select, proceed } = formEL.elements;
+const { firstName, lastName, email, amount, } = formEL.elements;
+const formELData = new FormData(formEL);
 
 // greeting Donar
-formEL.addEventListener("submit", (event) => {
+const handleSubmit = (event) => {
   event.preventDefault();
-  allDivEl.classList.add("hidden");
-  pEl.innerText = `✅Thanks for your donation ${firstNameEl.value}.`;
-});
+  mainEl.classList.add("hidden");
+  let greetStatement = "";
+  if (amount[0].checked) {
+    greetStatement = `You have donated ₹${amount[0].value} ✅`;
+  } else if (amount[1].checked) {
+    greetStatement = `You have donated ₹${amount[1].value} ✅`;
+  } else if (amount[2].checked) {
+    greetStatement = `You have donated ₹${amount[2].value} ✅`;
+  } else if (amount[3].checked) {
+    greetStatement = `You have donated ₹${amount[3].value} ✅`;
+  }
+  pEl.innerText = `${greetStatement}
+  ✨Thanks for your donation ${firstName.value}.`;
+
+  // sending data to backend
+  
+  console.log([...formELData.entries(formEL)]); 
+};
+
+formEL.addEventListener("submit", handleSubmit);
