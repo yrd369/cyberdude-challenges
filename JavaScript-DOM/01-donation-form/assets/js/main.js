46e1f4dd69d7383e323ff83d11be72aa34f61c40
yrd369 <yogharajdhayalnamburajan@gmail.com>
2023-12-22 11:28:34 +0530

    feat: add feature to get input data in JSON format

--- JavaScript-DOM/01-donation-form/assets/js/main.js
+++ JavaScript-DOM/01-donation-form/assets/js/main.js
@@ -1,29 +1,37 @@
+// getting elements
 const pEl = document.querySelector("p#greet");
-const mainEl = document.querySelector("form");
 const formEL = document.forms.donation;
-const { firstName, lastName, email, amount, } = formEL.elements;
-const formELData = new FormData(formEL);
+const { firstName, lastName, email, amount } = formEL.elements;
 
 // greeting Donar
 const handleSubmit = (event) => {
   event.preventDefault();
-  mainEl.classList.add("hidden");
-  let greetStatement = "";
-  if (amount[0].checked) {
-    greetStatement = `You have donated ₹${amount[0].value} ✅`;
-  } else if (amount[1].checked) {
-    greetStatement = `You have donated ₹${amount[1].value} ✅`;
-  } else if (amount[2].checked) {
-    greetStatement = `You have donated ₹${amount[2].value} ✅`;
-  } else if (amount[3].checked) {
-    greetStatement = `You have donated ₹${amount[3].value} ✅`;
-  }
+  formEL.classList.add("hidden");
+  let greetStatement = "✅ You have donated ₹";
+
+  // printing value by array method
+  const inputVal = Array.from(amount).find((val) => {
+    return val.checked;
+  });
+  greetStatement += ` ${inputVal.value}`;
+
+  // printing value by if-else
+  // if (amount[0].checked) {
+  //   greetStatement = `✅ You have donated ₹${amount[0].value}`;
+  // } else if (amount[1].checked) {
+  //   greetStatement = `✅ You have donated ₹${amount[1].value}`;
+  // } else if (amount[2].checked) {
+  //   greetStatement = `✅ You have donated ₹${amount[2].value}`;
+  // } else if (amount[3].checked) {
+  //   greetStatement = `✅ You have donated ₹${amount[3].value}`;
+  // }
   pEl.innerText = `${greetStatement}
   ✨Thanks for your donation ${firstName.value}.`;
 
-  // sending data to backend
-  
-  console.log([...formELData.entries(formEL)]); 
+  // getting input value
+  const formELData = [...new FormData(formEL)];
+  const objFormData = Object.fromEntries(formELData);
+  console.log(JSON.stringify(objFormData));
 };
 
 formEL.addEventListener("submit", handleSubmit);
