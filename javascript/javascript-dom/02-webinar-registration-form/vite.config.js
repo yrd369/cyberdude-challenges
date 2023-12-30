9356c33b4bd18e0093c456e2ab114124c708f55b
yrd369 <yogharajdhayalnamburajan@gmail.com>
2023-12-30 21:43:51 +0530

    fix: added vite config file

---
+++ javascript/javascript-dom/02-webinar-registration-form/vite.config.js
@@ -0,0 +1,4 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+  base: "https://github.com/yrd369/cyberdude-challenges/tree/master/javascript/javascript-dom/02-webinar-registration-form",
+});
