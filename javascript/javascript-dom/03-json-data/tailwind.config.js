03c243ee35f19e4f616a54cbae00f63b1d0af2bb
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-01-07 23:51:33 +0530

    feat: create database in json

--- javascript/javascript-dom/03-json-data/tailwind.config.js
+++ javascript/javascript-dom/03-json-data/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./**/*.js", "./src/**/*.{js,ts,jsx,tsx,json}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
