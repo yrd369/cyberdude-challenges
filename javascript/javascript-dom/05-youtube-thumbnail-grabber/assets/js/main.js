98ba4d3903e226db0a8a1778f4fddf54c5814c1a
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-01-08 22:37:32 +0530

    feat: getting thumbnail from video link

---
+++ javascript/javascript-dom/05-youtube-thumbnail-grabber/assets/js/main.js
@@ -0,0 +1,9 @@
+const linkEl = document.getElementById("link");
+const formEl = document.getElementById("form");
+const imgEl = document.getElementById("img")
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const videoId = linkEl.value.split("=")[1];
+  let imageUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+  imgEl.setAttribute("src",imageUrl)
+});
