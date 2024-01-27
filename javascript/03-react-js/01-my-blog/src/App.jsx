2ff84b53ffee75a0af6c7e4233fb80cf849a4a08
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-01-27 09:55:26 +0530

    chore: project initialized

---
+++ javascript/03-react-js/01-my-blog/src/App.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./pages/layouts/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
