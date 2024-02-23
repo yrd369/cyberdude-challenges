e18beb3d0a24e1c05d9b549a26df346587cefebb
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-02-23 10:38:50 +0530

    fix: bugs

--- javascript/05-firebase/01-cookease/src/firebase/index.js
+++ javascript/05-firebase/01-cookease/src/firebase/index.js
@@ -1,29 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
 } = import.meta.env;
 const firebaseConfig = {
-  apiKey: "AIzaSyBr3Iv8TCLUx0XKRkMCfvkDPrHWFSHcUgI",
-  authDomain: "cookease-57cac.firebaseapp.com",
-  projectId: "cookease-57cac",
-  storageBucket: "cookease-57cac.appspot.com",
-  messagingSenderId: "674473447381",
-  appId: "1:674473447381:web:0adaee165af6134544f75c",
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
-// console.log(db);
+// console.log(import.meta.env.VITE_APP_ID);
